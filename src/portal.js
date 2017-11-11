9a5fbc39af2a8638874f97197f67d85309fb430f
Francisco Grau <grau.fran@gmail.com>
2017-11-11 14:08:28 +0100

    Made compatible with UglifyJS
    Removed ES-2015 syntax

--- src/portal.js
+++ src/portal.js
@@ -1,51 +1,51 @@
-import PortalBus from './portal-bus';
+import PortalBus from './portal-bus'
 
 export default {
   name: 'portal',
   props: ['to', 'mountTo'],
-  mounted() {
+  mounted: function () {
     this.$nextTick(this.setup)
   },
-  updated() {
-    this.sendUpdate();
+  updated: function () {
+    this.sendUpdate()
   },
-  destroyed() {
-    PortalBus.$emit(`update:${this.to}`, null);
+  destroyed: function () {
+    PortalBus.$emit('update:' + this.to, null)
   },
   methods: {
-    sendUpdate() {
-      PortalBus.$emit(`update:${this.to}`, this.$slots.default);
+    sendUpdate: function () {
+      PortalBus.$emit('update:' + this.to, this.$slots.default)
     },
-    setup() {
+    setup: function () {
       if (this.mountTo) {
-        let targetEl = document.getElementById(this.mountTo);
+        var targetEl = document.getElementById(this.mountTo)
         if (!targetEl) {
-          console.warn(`Target Element with id '${this.mountTo}' not found!`);
+          console.warn('Target Element with id ' + this.mountTo + ' not found!')
           return
         }
 
-        let PortalTarget = this.$options.components.portalTarget;
-        let portalTarget = new PortalTarget({
+        var PortalTarget = this.$options.components.portalTarget
+        var portalTarget = new PortalTarget({
           parent: this,
           propsData: {
             name: this.to
           },
-        });
-        portalTarget.$mount(targetEl);
+        })
+        portalTarget.$mount(targetEl)
 
-        this.$nextTick(portalTarget.update(this.$slots.default));
+        this.$nextTick(portalTarget.update(this.$slots.default))
 
       } else {
-        this.$nextTick(this.sendUpdate);
+        this.$nextTick(this.sendUpdate)
       }
     },
   },
-  render(h) {
-    return  h('div',
+  render: function (h) {
+    return h('div',
       {
         style: {display: 'none'},
         ref: 'portalWrapper'
       }
     )
   }
-};
+}
