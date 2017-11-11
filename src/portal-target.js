c99872312fff2beec4f230899bfdc64901cd38ce
Francisco Grau <grau.fran@gmail.com>
2017-11-11 14:09:42 +0100

    Made compatible with UglifyJS
    Replaced ES-2015 syntax

--- src/portal-target.js
+++ src/portal-target.js
@@ -1,25 +1,25 @@
-import PortalBus from './portal-bus';
+import PortalBus from './portal-bus'
 
 export default {
   name: 'portalTarget',
   props: ['name'],
-  created() {
-    this.passengers = null;
-    PortalBus.$on(`update:${this.name}`, this.update)
+  created: function () {
+    this.passengers = null
+    PortalBus.$on('update:' + this.name, this.update)
   },
-  beforeDestroy() {
-    PortalBus.$off(`update:${this.name}`, this.update)
+  beforeDestroy: function () {
+    PortalBus.$off('update:' + this.name, this.update)
   },
   methods: {
-    update(passengers) {
-      this.passengers = passengers ? passengers : null;
+    update: function (passengers) {
+      this.passengers = passengers ? passengers : null
       this.$forceUpdate()
     }
   },
-  render(h) {
-    let children = this.passengers ? this.passengers : null;
+  render: function (h) {
+    var children = this.passengers ? this.passengers : null
     return h('div', {
-      class: { 'portal-target': true }
+      class: {'portal-target': true}
     }, children)
   }
-};
+}
